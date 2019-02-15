b88832a62b50d20dfd8bac08bcdaac8dce13355c
Emily <emplums@github.com>
2019-02-15 06:13:33 -0800

    add Contributors component

--- src/components.js
+++ src/components.js
@@ -1,6 +1,7 @@
 export {default as BoxShadow} from './BoxShadow'
 export {default as ClipboardCopy} from './ClipboardCopy'
 export {default as CodeExample} from './CodeExample'
+export {default as Contributors} from './Contributors'
 export {default as DetailsDialog} from './DetailsDialog'
 export {default as Frame} from './Frame'
 export {default as Header} from './Header'
