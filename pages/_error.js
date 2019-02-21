d742a251ff821bf0a1ac93eeb1da6d4402201830
Emily <emplums@github.com>
2019-02-20 16:38:40 -0800

    fix redirect path

--- pages/_error.js
+++ pages/_error.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Heading} from '@primer/components'
-import {redirectTrailingSlash} from '../docs/redirect'
+import {redirectTrailingSlash} from '../src/redirect'
 
 export default class Error extends React.Component {
   static getInitialProps(context) {
