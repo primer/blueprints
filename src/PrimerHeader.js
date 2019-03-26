0911ca27ec17274590269c16f8b6c5f67b314056
emplums <emplums@github.com>
2019-03-26 10:18:48 -0700

    remove overview links for now

--- src/PrimerHeader.js
+++ src/PrimerHeader.js
@@ -8,20 +8,17 @@ const PrimerHeader = ({root}) => {
     <Header title="Primer" subtitle='Blueprints' root={root}>
       <Link color='blue.2' fontWeight='bold' href='https://primer.style/news'>What's New</Link>
       <NavDropdown title='Design'>
-        <NavItem href="https://primer.style#design">Overview</NavItem>
         <NavItem href="https://primer.style/design">Interface Guidelines</NavItem>
         <NavItem href="https://octicons.github.com/">Icons</NavItem>
         <NavItem href="https://github.com/primer/presentations">Presentations</NavItem>
       </NavDropdown>
       <NavDropdown title='Development'>
-        <NavItem href="https://primer.style#development">Overview</NavItem>
         <NavItem href="https://primer.style/css">Primer CSS</NavItem>
         <NavItem href="https://primer.style/components">Primer Components</NavItem>
         <NavItem href="https://github.com/primer/deploy">Deploy</NavItem>
         <NavItem href="https://primer.style/css/tools/prototyping">Prototyping</NavItem>
       </NavDropdown>
       <NavDropdown direction='sw' title='Tools'>
-        <NavItem href="https://primer.style#tools">Overview</NavItem>
         <NavItem href="https://primer.style/css/tools/atom-packages">Atom packages</NavItem>
         <NavItem href="https://primer.style/css/tools/docset">Docs set</NavItem>
         <NavItem href="https://primer.style/css/tools/local-primer">GitHub local environment</NavItem>
