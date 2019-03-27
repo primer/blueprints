c7e4f7753daa0fa69168a6c6f079569172f1be57
emplums <emplums@github.com>
2019-03-27 06:40:38 -0700

    remove tools from responsive jumpnav

--- src/ResponsiveJumpNav.js
+++ src/ResponsiveJumpNav.js
@@ -20,26 +20,23 @@ const ResponsiveJumpNav = () => {
         </PrimerLink>
       </BorderBox>
       <BorderBox py={3} borderColor="gray.2" borderTop={0} borderLeft={0} borderRight={0} borderBottom={1}>
+        <PrimerLink color="black" fontWeight="bold" my={2} href="https://primer.style/team">
+          Team
+        </PrimerLink>
+      </BorderBox>
+      <BorderBox py={3} borderColor="gray.2" borderTop={0} borderLeft={0} borderRight={0} borderBottom={1}>
         <NavDropdown.Responsive color="black" title="Design">
           <DropdownLink href="https://primer.style/design">Interface Guidelines</DropdownLink>
           <DropdownLink href="https://octicons.github.com/">Icons</DropdownLink>
           <DropdownLink href="https://github.com/primer/presentations">Presentations</DropdownLink>
         </NavDropdown.Responsive>
       </BorderBox>
-      <BorderBox py={3} borderColor="gray.2" borderTop={0} borderLeft={0} borderRight={0} borderBottom={1}>
+      <Box pt={3}>
         <NavDropdown.Responsive color="black" title="Development">
           <DropdownLink href="https://primer.style/css">Primer CSS</DropdownLink>
           <DropdownLink href="https://primer.style/components">Primer Components</DropdownLink>
           <DropdownLink href="https://github.com/primer/deploy">Deploy</DropdownLink>
           <DropdownLink href="https://primer.style/css/tools/prototyping">Prototyping</DropdownLink>
-        </NavDropdown.Responsive>
-      </BorderBox>
-      <Box pt={3}>
-        <NavDropdown.Responsive color="black" title="Tools">
-          <DropdownLink href="https://primer.style/css/tools/atom-packages">Atom packages</DropdownLink>
-          <DropdownLink href="https://primer.style/css/tools/docset">Docs set</DropdownLink>
-          <DropdownLink href="https://primer.style/css/tools/local-primer">GitHub local environment</DropdownLink>
-          <DropdownLink href="https://primer.style/css/tools/linting">Linting</DropdownLink>
         </NavDropdown.Responsive>
       </Box>
     </BorderBox>
