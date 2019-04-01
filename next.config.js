c31343b45cd68b69e4894cab6432dc22942c80cb
emplums <emplums@github.com>
2019-04-01 11:45:31 -0700

    remove sass loader

--- next.config.js
+++ next.config.js
@@ -2,13 +2,4 @@ const {join, resolve} = require('path')
 const withSass = require('@zeit/next-sass')
 const configure = require('./lib/config')
 
-module.exports = configure(
-  withSass({
-    sassLoaderOptions: {
-      includePaths: [
-        resolve(__dirname, '../modules'),
-        resolve(__dirname, 'node_modules')
-      ]
-    }
-  })
-)
+module.exports = configure()
