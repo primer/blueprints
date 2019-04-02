7fd1bf26cc81aa52fb851afe42182cc7f86c7c88
emplums <emplums@github.com>
2019-04-02 14:09:05 -0700

    progress

--- src/ResponsiveSearchInput.js
+++ src/ResponsiveSearchInput.js
@@ -4,14 +4,18 @@ import React, {useState} from 'react'
 import styled from 'styled-components'
 import SearchInput from './SearchInput'
 
-//
-const openStyles = `
-  width: 100%;
-  position: absolute;
-  left: 0;
-  top: 0;
-  background: #1b1f23;
-  padding: 16px 0 16px 16px;
+
+const Wrapper = styled.div`
+  display: flex;
+  position: relative;
+  ${props => props.open && `
+    width: 100%;
+    position: absolute;
+    right: 0;
+    top: 0;
+    background: #1b1f23;
+    padding: 16px 0 16px 16px;
+  `};
 `
 
 const StyledButton = styled.button`
@@ -27,6 +31,7 @@ const StyledButton = styled.button`
   border-color: #586069;
   border-radius: 3px;
   height: 38px;
+  right: 0px;
 
   &:hover,
   &:focus {
@@ -46,12 +51,12 @@ const ResponsiveSearchInput = props => {
     setOpen(!open)
   }
   return (
-    <Box as={Flex} css={open ? openStyles : ''}>
+    <Wrapper open={open}>
       {open && <SearchInput mr={3} width='100%' {...props} />}
       <StyledButton onClick={handleClick}>
         <StyledOcticon icon={open ? X : Search} />
       </StyledButton>
-    </Box>
+    </Wrapper>
   )
 }
 
