9bff10a815a7120cac81d19a076ddd33e5e8694d
emplums <emplums@github.com>
2019-04-02 17:06:51 -0700

    lint

--- src/Search.js
+++ src/Search.js
@@ -1,18 +1,9 @@
 // downshift applies htmlFor for the label, but eslint doesn't recognize that
 // also disabling the specific line causes some weirdness when running lint for syntax
 /* eslint-disable jsx-a11y/label-has-for */
-import React, {useState} from 'react'
-import Router from 'next/router'
-import lunr from 'lunr'
-import {Relative, Box, Text} from '@primer/components'
-import SearchItem from './SearchItem'
-import Downshift from 'downshift'
-import SearchInput from './SearchInput'
-import SearchResults from './SearchResults'
-import ResponsiveSearchInput from './ResponsiveSearchInput'
+import React from 'react'
 
-
-function Search({subfolder, documents}) {
+function Search() {
   return (
     <div>
       hi
