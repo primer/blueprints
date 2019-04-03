607b830c9797fc1aaae321b27547e1b95373b1cd
emplums <emplums@github.com>
2019-04-02 23:02:24 -0700

    use regular link instead of next link

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import {MarkGithub, ChevronRight} from '@githubprimer/octicons-react'
-import {Text, Flex, Sticky, BorderBox, Box, StyledOcticon} from '@primer/components'
-import Link from './Link'
+import {Text, Link, Flex, Sticky, BorderBox, Box, StyledOcticon} from '@primer/components'
 import Search from './Search'
 
 const BoxShadow = styled(Box)`
