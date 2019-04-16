78abb49e68cc6b6cf22785fcd070352a0a626430
emplums <emplums@github.com>
2019-04-16 11:10:00 -0700

    remove console.log and comment

--- src/NavList.js
+++ src/NavList.js
@@ -7,15 +7,9 @@ import SectionLink from './SectionLink'
 
 const {pageMap = new Map()} = Pages
 
-/**
- * A <NavList> renders a <Section.Link> for the given `path` and looks up the
- * path in the page tree. If a node is found, it renders a <NavLink> for each
- * of the node's children.
- */
 export default function NavList({path, currentPath}) {
   const node = pageMap.get(path)
   const children = node ? node.children.sort(nodeSort) : []
-  console.log('!!!')
   return (
     <BorderBox p={5} border={0} borderBottom={1} borderRadius={0} width="100%">
       <SectionLink next currentPath={currentPath} color="gray.9" href={path} mb={3} />
