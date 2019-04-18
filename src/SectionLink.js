cfaa0860d4c431f67058e014ce14ef987e3e560a
emplums <emplums@github.com>
2019-04-18 11:45:16 -0700

    SectionLink docs

--- src/SectionLink.js
+++ src/SectionLink.js
@@ -2,13 +2,17 @@ import React from 'react'
 import {Box, Link} from '@primer/components'
 import PageLink from './PageLink'
 
-const SectionLink = ({href, router, currentPath, next, ...rest}) => {
+const SectionLink = ({href, currentPath, next, ...rest}) => {
   const Tag = next ? PageLink : Link
   return (
-    <Box mb={3} color='gray.9' {...rest}>
-      <Tag href={href} {...rest} fontSize={2} fontWeight={currentPath.startsWith(href) ? 'bold' : null} />
+    <Box mb={3} {...rest}>
+      <Tag href={href} color='gray.9' {...rest} fontSize={2} fontWeight={currentPath.startsWith(href) ? 'bold' : null} />
     </Box>
   )
+}
+
+SectionLink.defaultProps = {
+  next: false
 }
 
 export default SectionLink
