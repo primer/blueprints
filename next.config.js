44cdbf0a3cc8ec770eb05e9b803749440cf96ea0
Shawn Allen <shawn.allen@github.com>
2019-06-03 16:43:50 -0700

    add target: "serverless" to next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withNextPages = require('@primer/next-pages/plugin')
 const configure = require('./lib/config')
 
 module.exports = configure(withNextPages({
+  target: 'serverless',
   env: {
     GOOGLE_ANALYTICS_UA: 'UA-126681523-1'
   }
