418039a119f9ef2b97b76ec4f33ed4854c520851
Shawn Allen <shawn.allen@github.com>
2019-06-03 16:35:15 -0700

    lint

--- src/analytics.js
+++ src/analytics.js
@@ -1,7 +1,7 @@
 import Router from 'next/router'
 import withGA from 'next-ga'
 
-const {id: GOOGLE_ANALYTICS_UA} = process.env
+const {GOOGLE_ANALYTICS_UA: id} = process.env
 
 export default function withAnalytics(Component) {
   return id ? withGA(id, Router)(Component) : Component
