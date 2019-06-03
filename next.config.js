1a4848bb7e7b6fb00c26bdfc86c693aa06c526c0
Shawn Allen <shawn.allen@github.com>
2019-06-03 16:28:28 -0700

    add Primer Components analytics ID to next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 const withNextPages = require('@primer/next-pages/plugin')
 const configure = require('./lib/config')
 
-module.exports = configure(withNextPages())
+module.exports = configure(withNextPages({
+  env: {
+    GOOGLE_ANALYTICS_UA: 'UA-126681523-1'
+  }
+}))
